ca9f68c8c03c5bf18ff54d7c91e3df7aba8bdac6
Sigmame <i@sigma.me>
2024-12-27 03:24:39 +0800

    Initial Commit

---
+++ PanoramaViewer/PanoramaViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PanoramaViewerApp.swift
+//  PanoramaViewer
+//
+//  Created by sigma on 2024/12/27.
+//
+
+import SwiftUI
+
+@main
+struct PanoramaViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
